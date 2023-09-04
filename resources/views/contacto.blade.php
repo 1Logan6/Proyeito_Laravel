9e4184b66f5da6bfd985ef797b00e5f2e88f888e
logan <logan.rivera5140@alumnos.udg.mx>
2023-09-04 16:45:17 -0600

    primera pagina con laravel

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+    <h1>Formulario de Contacto</h1>
+    <form action="">
+        <label for="correo"></label><br>
+        <input type="email" name="correo"><br>
+        <label name="comentario">Comenta</label><br>
+        <textarea name="comentario" id="comentario" cols="30" rows="30"></textarea>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
